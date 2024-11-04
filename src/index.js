ff5a486360cf448e6b60dda5e0fc329ffdb1cde0
Zakari Ka <zakari_kaddour@live.fr>
2024-11-04 14:29:48 +0100

    ajout page about

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './sass/main.scss';
 import Home from './pages/Home';
+import Error from './pages/Error';
+import About from './pages/About'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,9 +12,9 @@ root.render(
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/Logements/:id" element={<Logements />} />
+        {/* <Route path="/Logements/:id" element={<Logements />} /> */}
         <Route path='/About' element={<About />} />
-        <Route path='/*' element={<Error />} /> */}
+        <Route path='/*' element={<Error />} />
       </Routes>
     </Router>
   </React.StrictMode>
